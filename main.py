590de06ef35e10d156b334df8ee6a68e2deacdf3
sopnotrader101 <sopnotrader101@gmail.com>
2026-06-29 15:53:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ TELEGRAM_API_ID = int(os.environ.get("TELEGRAM_API_ID", "31955122"))
 TELEGRAM_API_HASH = os.environ.get("TELEGRAM_API_HASH", "4f3e7f6d8250dc14c21ae58642fcbcc9")
 
 # Bot Token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8198086071:AAE-H_Ax-bacCpMRKA3kJpB0WMJnRO7-77E")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8467555740:AAEqBWpnxN7aQrc0TgD15W_LDeiDSOllMcY")
 
 # Required channel for all users
 REQUIRED_CHANNEL = "@BGTUpdate"
